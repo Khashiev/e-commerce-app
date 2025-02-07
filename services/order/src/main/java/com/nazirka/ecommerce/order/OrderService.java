67177d58bdabc6444ed4274e9ae6a5d40a4df7f3
Khashiev <nxashiev@gmail.com>
2025-02-07 22:51:29 +0300

    add kafka and implement some order service functions

--- services/order/src/main/java/com/nazirka/ecommerce/order/OrderService.java
+++ services/order/src/main/java/com/nazirka/ecommerce/order/OrderService.java
@@ -2,13 +2,19 @@ package com.nazirka.ecommerce.order;
 
 import com.nazirka.ecommerce.customer.CustomerClient;
 import com.nazirka.ecommerce.exception.BusinessException;
+import com.nazirka.ecommerce.kafka.OrderConfirmation;
+import com.nazirka.ecommerce.kafka.OrderProducer;
 import com.nazirka.ecommerce.orderline.OrderLineRequest;
 import com.nazirka.ecommerce.orderline.OrderLineService;
 import com.nazirka.ecommerce.product.ProductClient;
 import com.nazirka.ecommerce.product.PurchaseRequest;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -18,13 +24,14 @@ public class OrderService {
     private final ProductClient productClient;
     private final OrderMapper mapper;
     private final OrderLineService orderLineService;
+    private final OrderProducer orderProducer;
 
     public Integer createOrder(@Valid OrderRequest request) {
         var customer = customerClient.findCustomerById(request.customerId())
                 .orElseThrow(() -> new BusinessException("Cannot create customer with id " +
                         request.customerId()));
 
-        this.productClient.purchaseProducts(request.products());
+        var purchasedProducts = this.productClient.purchaseProducts(request.products());
 
         var order = repository.save(mapper.toOrder(request));
 
@@ -39,6 +46,31 @@ public class OrderService {
             );
         }
 
-        return null;
+        // todo start payment process
+
+        orderProducer.sendOrderConfirmation(
+                new OrderConfirmation(
+                        request.reference(),
+                        request.amount(),
+                        request.paymentMethod(),
+                        customer,
+                        purchasedProducts
+                )
+        );
+
+        return order.getId();
+    }
+
+    public List<OrderResponse> findAll() {
+        return repository.findAll()
+                .stream()
+                .map(mapper::fromOrder)
+                .collect(Collectors.toList());
+    }
+
+    public OrderResponse findById(Integer orderId) {
+        return repository.findById(orderId)
+                .map(mapper::fromOrder)
+                .orElseThrow(() -> new EntityNotFoundException("Order with id " + orderId + " not found"));
     }
 }
---
+++ services/order/src/main/java/com/nazirka/ecommerce/orderline/OrderLineResponse.java
@@ -0,0 +1,7 @@
+package com.nazirka.ecommerce.orderline;
+
+public record OrderLineResponse(
+        Integer id,
+        double quantity
+) {
+}
